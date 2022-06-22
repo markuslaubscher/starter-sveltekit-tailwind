85ab66c4f94872e175e5b498b7e29fc68b693d54
Markus Laubscher <markuslaubscher@gmail.com>
2022-06-22 17:15:25 +0200

    updated index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Starter Sveltekit Tailwind</title>
+	<meta name="description" content="Starter Sveltekit Tailwind" />
 </svelte:head>
 
 <h1 class="text-3xl font-bold underline">
